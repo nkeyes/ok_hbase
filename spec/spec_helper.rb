9f094f2b96698abfb743a127cbfa0d34dc4a38c5
Nathan Keyes <keyes@okcupidlabs.com>
2013-05-12 11:58:29 -0700

    fix simpleco usage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,19 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'support'))
 
+# needs to be at the top
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/thrift/"
+end
+
 Bundler.require :default, :test
 
-require 'simplecov'
+
 require 'rubygems'
 require 'bundler'
 require 'awesome_print'
 
 require 'ok-hbase'
 
-SimpleCov.start
+
