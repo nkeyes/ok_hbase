8492b37edc9002e63c8d7f0dfbf57e7a210711b8
Nathan Keyes <keyes@okcupidlabs.com>
2013-05-12 21:59:37 -0700

    add table.create tests

--- spec/ok-hbase/connection_spec.rb
+++ spec/ok-hbase/connection_spec.rb
@@ -2,6 +2,76 @@ require 'spec_helper'
 
 module OkHbase
   describe Connection do
+    describe ".create_table" do
+      let(:conn) { Connection.new auto_connect: true, timeout: 60 }
+
+      it "should create tables with the right column families" do
+        name = "ok-hbase_test_table"
+        column_families = {
+            'a' => {
+                'max_versions' => 5,
+                'compression' => 'GZ',
+                'in_memory' => true,
+                'bloom_filter_type' => 'ROW',
+                'block_cache_enabled' => true,
+
+                #TODO find out if these aren't being set properly or just aren't reported properly. 0 is the default
+                'bloom_filter_vector_size' => 0,
+                'bloom_filter_nb_hashes' => 0,
+
+                #TODO find out why this doesn't get reported properly. -1 is the default
+                'time_to_live' => -1
+            },
+            'b' => {
+                'max_versions' => 15,
+                'compression' => 'NONE',
+                'in_memory' => true,
+                'bloom_filter_type' => 'ROWCOL',
+                'block_cache_enabled' => true,
+
+                'bloom_filter_vector_size' => 0,
+                'bloom_filter_nb_hashes' => 0,
+
+                'time_to_live' => -1
+            }
+        }
+
+        expected_families = Hash[column_families.map { |cf, data| [cf, { 'name' => "#{cf}:" }.merge(data)] }]
+
+        #sanity check
+        conn.tables.should_not include(name)
+
+        conn.create_table(name, column_families)
+
+        conn.tables.should include(name)
+
+        table = conn.table(name)
+
+        table.families.should == expected_families
+
+        #cleanup
+        conn.delete_table(name, true)
+      end
+
+
+      it "should create tables with the right name" do
+        name = "ok-hbase_test_table"
+        column_families = {
+            'd' => {}
+        }
+
+        #sanity check
+        conn.tables.should_not include(name)
+
+        conn.create_table(name, column_families)
+
+        conn.tables.should include(name)
+
+        #cleanup
+        conn.delete_table(name, true)
+      end
+    end
+
     describe ".open" do
       let(:conn) { Connection.new }
 
