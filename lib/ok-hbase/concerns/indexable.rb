ad8a553a85ea0a5c5ba0f2b07fcf39f3b0c743cd
Nathan Keyes <keyes@okcupidlabs.com>
2013-05-18 15:04:23 -0700

    add support for automatic index writes

--- lib/ok-hbase/concerns/indexable.rb
+++ lib/ok-hbase/concerns/indexable.rb
@@ -15,6 +15,9 @@ module OkHbase
           pack_pattern = options[:pack_pattern]
           auto_create = options[:auto_create]
 
+          @@_indexes ||= {}.with_indifferent_access
+          @@_indexes[index_name] = options
+
           define_method index_name do |idx_options, &block|
             expected_option_keys = attributes[0...prefix_length]
             prefix_pack_pattern = pack_pattern[0...prefix_length].join('')
@@ -28,10 +31,46 @@ module OkHbase
             scan(row_prefix: row_prefix, &block)
           end
 
+          define_method :put do |row_key, data, timestamp = nil|
+            batch = self.batch(timestamp)
+
+            batch.transaction do |batch|
+              batch.put(row_key, data)
+              batch.send_batch()
+              @@_indexes.each_pair do |index_name, options|
+                next unless options[:auto_create]
+                row = self.class.row_class.new table: self, default_column_family: self.class.default_column_family, raw_data: data
+
+                row_key_components = options[:attributes].map do |attribute|
+
+                  value = if attribute == :index_id
+                    options[:index_id]
+                  else
+                    row.raw_data[attribute] || row.send(attribute)
+                  end
+
+                  ap attribute => value
+
+
+
+                  # coerce booleans to ints for packing
+                  value = 1 if value == 'true'
+                  value = 0 if value == 'false'
+
+                  # coerce hbase i64s to Fixnum, Bignum
+                  value = value.unpack('Q>').first if value.is_a?(String)
+
+                  value
+                end
+                ap row_key_components: row_key_components
+                index_row_key = row_key_components.pack(options[:pack_pattern].join(''))
+
+                batch.put(index_row_key, data)
+              end
+            end
+          end
         end
       end
-
-
     end
   end
 end
