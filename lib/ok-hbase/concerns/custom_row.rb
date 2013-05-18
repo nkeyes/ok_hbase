ade8640e4b6e69f52e925d51091ed915f0afa386
Nathan Keyes <keyes@okcupidlabs.com>
2013-05-18 15:05:16 -0700

    refactoring CustomRow module

--- lib/ok-hbase/concerns/custom_row.rb
+++ lib/ok-hbase/concerns/custom_row.rb
@@ -4,20 +4,20 @@ module OkHbase
       extend ActiveSupport::Concern
 
       def row(row_key, columns = nil, timestamp = nil, include_timestamp = false)
-        @@row_class.new table: self, data: super
+        @@_row_class.new table: self, default_column_family: self.class.default_column_family, raw_data: super
       end
 
       def rows(row_keys, columns = nil, timestamp = nil, include_timestamp = false)
-        super.map.with_index! { |data, i| self.class.row_class.new table: self, row_key: row_keys[i], default_column_family: self.class.default_column_family, data: data }
+        super.map.with_index! { |data, i| self.class.row_class.new table: self, row_key: row_keys[i], default_column_family: self.class.default_column_family, raw_data: data }
       end
 
       def scan(opts={})
-        super {|row_key, data| yield self.class.row_class.new(table: self, row_key: row_key, default_column_family: self.class.default_column_family, data: data) }
+        super {|row_key, data| yield self.class.row_class.new(table: self, row_key: row_key, default_column_family: self.class.default_column_family, raw_data: data) }
       end
 
       module ClassMethods
         def row_class
-          @@row_class
+          @@_row_class
         end
 
         def default_column_family
@@ -25,7 +25,7 @@ module OkHbase
         end
 
         def use_row_class(klass)
-          @@row_class = klass
+          @@_row_class = klass
         end
 
         def use_default_column_family(column_family)
