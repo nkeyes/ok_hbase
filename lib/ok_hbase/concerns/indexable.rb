2cc60ba0f0456b5c8b3ab8f416731a987bdd6113
Nathan Keyes <keyes@okcupidlabs.com>
2013-05-21 10:31:43 -0700

    add index accessibility methods

--- lib/ok_hbase/concerns/indexable.rb
+++ lib/ok_hbase/concerns/indexable.rb
@@ -15,8 +15,43 @@ module OkHbase
           pack_pattern = options[:pack_pattern]
           auto_create = options[:auto_create]
 
-          @@_indexes ||= {}.with_indifferent_access
+          @@_indexes ||= {}
+          @@_indexes = @@_indexes.with_indifferent_access
           @@_indexes[index_name] = options
+
+          define_method :indexes do
+            @@_indexes
+          end
+
+
+          define_method :key_for_index do |index_name, data|
+
+            options = @@_indexes[index_name]
+            row = self.class.row_class.new table: self, default_column_family: self.class.default_column_family, raw_data: data
+
+            row_key_components = options[:attributes].map do |attribute|
+
+              value = if attribute == :index_id
+                options[:index_id]
+              else
+                row.raw_data[attribute] || row.send(attribute)
+              end
+
+              # coerce booleans to ints for packing
+              value = 1 if value == true
+              value = 0 if value == false
+
+              # coerce hbase i64s to Fixnum, Bignum
+              value = value.unpack('Q>').first if value.is_a?(String)
+
+
+              value
+            end
+
+            row_key_components.pack(options[:pack_pattern].join(''))
+
+
+          end
 
           define_method index_name do |idx_options, &block|
             expected_option_keys = attributes[0...prefix_length]
@@ -32,38 +67,11 @@ module OkHbase
           end
 
           define_method :put do |row_key, data, timestamp = nil|
-            batch = self.batch(timestamp)
-
-            batch.transaction do |batch|
-              batch.put(row_key, data)
-              batch.send_batch()
+            self.batch(timestamp).transaction do |batch|
               @@_indexes.each_pair do |index_name, options|
                 next unless options[:auto_create]
-                row = self.class.row_class.new table: self, default_column_family: self.class.default_column_family, raw_data: data
 
-                row_key_components = options[:attributes].map do |attribute|
-
-                  value = if attribute == :index_id
-                    options[:index_id]
-                  else
-                    row.raw_data[attribute] || row.send(attribute)
-                  end
-
-                  ap attribute => value
-
-
-
-                  # coerce booleans to ints for packing
-                  value = 1 if value == 'true'
-                  value = 0 if value == 'false'
-
-                  # coerce hbase i64s to Fixnum, Bignum
-                  value = value.unpack('Q>').first if value.is_a?(String)
-
-                  value
-                end
-                ap row_key_components: row_key_components
-                index_row_key = row_key_components.pack(options[:pack_pattern].join(''))
+                index_row_key = key_for_index(index_name, data)
 
                 batch.put(index_row_key, data)
               end
