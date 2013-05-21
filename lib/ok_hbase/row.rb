af0bd0f164df24ad9df38f8b5919d6fb5601309e
Nathan Keyes <keyes@okcupidlabs.com>
2013-05-21 10:32:20 -0700

    better handling of default data for a row

--- lib/ok_hbase/row.rb
+++ lib/ok_hbase/row.rb
@@ -7,33 +7,45 @@ module OkHbase
       opts = opts.with_indifferent_access
 
       raise ArgumentError.new "'table' must be an OkHBase::Table" unless opts[:table] && opts[:table].is_a?(OkHbase::Table)
+      @default_column_family = opts[:default_column_family]
 
       @table = opts[:table]
 
       @row_key = opts[:row_key]
-      @raw_data = opts[:raw_data] || {}
-      @default_column_family = opts[:default_column_family]
+      @raw_data = {}
+      opts[:raw_data].each_pair do |k, v|
 
+        send(:"#{k}=", v)
+      end
     end
 
     def save!
-      raise ArgumentError.new "row_key must be a non-empty string" unless !@row_key.blank? && @row_key.is_a?(String)
+      #raise ArgumentError.new "row_key must be a non-empty string" unless !@row_key.blank? && @row_key.is_a?(String)
 
       encoded_data = Hash[@raw_data.map { |k, v| [k, _encode(v)] }]
       @table.put(@row_key, encoded_data, @timestamp)
     end
 
     def method_missing(method, *arguments, &block)
+
       if method.to_s[-1, 1] == '='
 
         key = method[0...-1]
         val = arguments.last
-        key = "#{@default_column_family}:#{key}" unless key.to_s.include? ':'
+        unless key.to_s.include? ':'
+          key = "#{@default_column_family}:#{key}"
+        else
+        end
 
-        return @raw_data[key] = val
+        ret_val = @raw_data[key] = val
+
+        ret_val
       else
 
-        key = "#{@default_column_family}:#{method}" unless method.to_s.include? ':'
+        unless method.to_s.include? ':'
+          key = "#{@default_column_family}:#{method}"
+        else
+        end
         return @raw_data[key]
       end
 
