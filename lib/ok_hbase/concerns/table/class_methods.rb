b136efe42a4a7abc637f1f3611a354549b807dce
Nathan Keyes <keyes@okcupidlabs.com>
2013-05-22 15:27:49 -0700

    class_methods refactor

--- lib/ok_hbase/concerns/table/class_methods.rb
+++ lib/ok_hbase/concerns/table/class_methods.rb
@@ -4,31 +4,8 @@ module OkHbase
       module ClassMethods
         extend ActiveSupport::Concern
 
-
-
         module ClassMethods
           include OkHbase::Concerns::Table
-
-          @@table_name ||= nil
-          @@connection ||= nil
-
-          def table_name
-            @@table_name
-          end
-
-          def table_name=(val)
-            @@table_name = val
-          end
-
-          def connection
-            @@connection
-          end
-
-          def connection=(val)
-            @@connection = val
-          end
-
-
         end
       end
     end
